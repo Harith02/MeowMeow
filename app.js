fd7488fc456563a1d9bda4f888962e10d4d3c022
Harith Eksan <qalsyaiful@gmail.com>
2025-08-17 17:57:13 +0800

    update: Update file paths and imports

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import TinderCard from "react-tinder-card";
-import "./App.css";
+import "./app.css";
 
 export default function App() {
   const [cats, setCats] = useState([]);
